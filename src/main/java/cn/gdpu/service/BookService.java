a0098d4233270338628bf26f2c87da6c903f544b
ws <ws5199@126.com>
2021-03-02 09:55:47 +0800

    the first time to commit

---
+++ src/main/java/cn/gdpu/service/BookService.java
@@ -0,0 +1,9 @@
+package cn.gdpu.service;
+
+/**
+ * @author:ws
+ * @Date:2021/3/2
+ * @description:learning
+ */
+public class BookService {
+}
---
+++ src/main/java/cn/gdpu/service/UserService.java
@@ -0,0 +1,12 @@
+package cn.gdpu.service;
+
+/**
+ * @author:ws
+ * @Date:2021/3/1
+ * @description:learning
+ */
+public class UserService {
+    public void test(){
+        System.out.println("userService....");
+    }
+}
